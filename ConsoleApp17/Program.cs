5656a54e074e86ec9e009490400745d94801b524
CRISTHIAN <CRISTHIAN@DESKTOP-LS2ABPT>
2023-10-06 17:23:49 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("*");
+            int i = 1;
+            while (i < num)
+            {
+                int aste = 1;
+                while (aste <= i*2)
+                {
+                    Console.Write("*");
+                    aste++;
+                }
+                i++;
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
